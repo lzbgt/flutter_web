7e1d65cd0c19d4137a0b21284974dacbd9e22dec
Bruce.Lu <lzbgt@icloud.com>
2020-12-14 10:53:45 +0800

    body v0.0

--- lib/pages/widgets/home_body.dart
+++ lib/pages/widgets/home_body.dart
@@ -4,7 +4,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import '../../bloc/home/home_bloc.dart';
 
 class HomeBodyWidget extends StatelessWidget {
-  const HomeBodyWidget();
+  HomeBodyWidget();
   final _shortcutsWidget = <Widget>[
     for (var i = 0; i < 10; i++)
       FlatButton(
@@ -30,28 +30,28 @@ class HomeBodyWidget extends StatelessWidget {
           ),
           ListTile(
             leading: Icon(Icons.text_fields),
-            title: TextFormField(
-              onTap: () {
-                print('editor tap');
-                showDialog(
-                  context: context,
-                  builder: (_) {
-                    return SizedBox(
+            title: TextFormField(onTap: () {
+              print('editor tap');
+              showDialog(
+                context: context,
+                builder: (_) {
+                  return SizedBox(
                       width: MediaQuery.of(context).size.width * 3 / 5,
                       height: MediaQuery.of(context).size.height * 3 / 5,
                       child: GridView.count(
                         crossAxisCount: 10,
                         children: List.generate(100, (index) {
-                            return Center(
-                              child: Text(
-                                'Item $index',
-                                style: Theme.of(context).textTheme.headline5,
-                              ),
-                              );
+                          return Center(
+                            child: Text(
+                              'Item $index',
+                              style: Theme.of(context).textTheme.headline5,
+                            ),
+                          );
                         }),
-                      )
-                  },   
-            ),
+                      ));
+                },
+              );
+            }),
             trailing: RaisedButton(
               onPressed: () {},
               child: Text('Send'),
